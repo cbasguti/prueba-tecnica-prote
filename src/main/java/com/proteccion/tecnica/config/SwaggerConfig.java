dde54766a5d0bf026ee8b379a494d93673b366a3
Sebas G <sgutierj@proteccion.com.co>
2024-08-06 11:46:46 -0500

    ajustes

--- src/main/java/com/proteccion/tecnica/config/SwaggerConfig.java
+++ src/main/java/com/proteccion/tecnica/config/SwaggerConfig.java
@@ -1,14 +1,5 @@
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+package com.proteccion.tecnica.config;
 
-@Configuration
 public class SwaggerConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
+    
 }
